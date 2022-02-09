e824bf5b5c2d034572fae5a4f73b6dd3837b8731
SeyaKen <75517657+SeyaKen@users.noreply.github.com>
2022-02-09 15:57:21 +0900

    Sign In Screen a little has made

--- lib/authentication/register.dart
+++ lib/authentication/register.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:foodpand_sellers_app/widget/custom_text_field.dart';
 
 class RegisterScreen extends StatefulWidget {
-  const RegisterScreen({ Key? key }) : super(key: key);
+  const RegisterScreen({Key? key}) : super(key: key);
 
   @override
   _RegisterScreenState createState() => _RegisterScreenState();
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  TextEditingController anyController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Container();
   }
 }
---
+++ lib/widget/custom_text_field.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  CustomTextField({
+    Key? key,
+    this.controller,
+    this.data,
+    this.hintText,
+    this.isObsecre
+  }) : super(key: key);
+
+  final TextEditingController? controller;
+  final IconData? data;
+  final String? hintText;
+  bool? isObsecre = true;
+  bool? enabled = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+      ),
+      padding: const EdgeInsets.all(8),
+      margin: const EdgeInsets.all(10),
+      child: TextFormField(
+        enabled: enabled,
+        controller: controller,
+        obscureText: isObsecre!, // パスワードモード（文字を隠す）
+        cursorColor: Theme.of(context).primaryColor,
+        decoration: InputDecoration(
+            // これをnoneにしないとfocusした時に
+            // 下にborderができちゃう
+            // やってみれば分かる
+            border: InputBorder.none,
+            prefixIcon: Icon(
+              data,
+              color: Colors.cyan,
+            ),
+            focusColor: Theme.of(context).primaryColor,
+            hintText: hintText,
+          )
+        )
+    );
+  }
+}
